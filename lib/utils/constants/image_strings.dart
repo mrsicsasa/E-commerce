6b4fd5f133ca2c5ed702b6fdcdb9ad572c16b1b9
Saša Mršić <sasamrsic20@gmail.com>
2025-08-18 23:30:35 +0200

    Added stating utils

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+  static const String darkAppLogo = "assets/logo/t-store-splash-logo-black.png";
+  static const String lightAppLogo =
+      "assets/logo/t-store-splash-logo-white.png";
+
+  // Social Logos
+  static const String google = "assets/logo/google-logo.png";
+  static const String facebook = "assets/logo/facebook-logo.png";
+}
